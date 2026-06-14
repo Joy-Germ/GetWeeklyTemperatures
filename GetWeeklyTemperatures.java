79195903020ced481bb2454e44e14a3fc9be9f38
Kylea Skinner <kyleaskinner@MacBookAir.lan>
2026-06-13 21:44:11 -0400

    fixed program bugs

--- GetWeeklyTemperatures.java
+++ GetWeeklyTemperatures.java
@@ -28,51 +28,51 @@ public class GetWeeklyTemperatures {
         int retrievalIndex;
         
         // Data Collection Phase
-for (int i = 0; i < NUM_DAYS_OF_WEEK; i++) {
-    System.out.print("Type the average temperature for " + daysOfWeek.get(i) + ": ")
+        for (int i = 0; i < NUM_DAYS_OF_WEEK; i++) {
+            System.out.print("Type the average temperature for " + daysOfWeek.get(i) + ": ");
 
-    // This will ensure that the day of the week positioned at i corresponds
-    // to the temperature positioned at i in the parallel ArrayList.
-    Double temp = scanner.nextDouble()
-    avgDailyTemp.add(temp)
-}
-
-// Average Calculation
-for (int i = 0; i < avgDailyTemp.size(); i++) {
-    // Using .size() allows the code to respond to the fact that the ArrayList
-    // is not fixed. The code will stay flexible and prevent bugs.
-    sumOfAverageDailyTemps = sumOfAverageDailyTemps + avgDailyTemp.get(i)
-}
-
-// Calculating this outside of the loop is necessary to keep the formula accurate.
-averageDailyTempForWeek = sumOfAverageDailyTemps / avgDailyTemp.size()
-
-// Data Retrieval Phase
-System.out.print("Type a day to view its average temperature, 'week' for the weekly summary, or 'done' to exit: ")
-userInput = scanner.nextLine()
-userInput = userInput.toLowerCase()
-
-while (!userInput.equals("done")) {
-
-    if (userInput.equals("week")) {
-        for (int i = 0; i < daysOfWeek.size(); i++) {
-            System.out.println(daysOfWeek.get(i) + ": " + avgDailyTemp.get(i))
+            // This will ensure that the day of the week positioned at i corresponds
+            // to the temperature positioned at i in the parallel ArrayList.
+            Double temp = scanner.nextDouble();
+            avgDailyTemp.add(temp);
         }
-        System.out.println("Weekly Average: " + averageDailyTempForWeek)
-    }
-    else if (daysOfWeek.contains(userInput)) {
-        retrievalIndex = daysOfWeek.indexOf(userInput)
-        System.out.println(daysOfWeek.get(retrievalIndex) + ": " + avgDailyTemp.get(retrievalIndex))
-    }
-    else {
-        System.out.println("Value not recognized. Double check spelling or type 'done' to exit.")
-    }
 
-    // Re-prompt for userInput to prevent infinite loop from occurring.
-    System.out.print("Enter a day, 'week', or 'done': ")
-    userInput = scanner.nextLine()
-    userInput = userInput.toLowerCase()
-}
+        // Average Calculation
+        for (int i = 0; i < avgDailyTemp.size(); i++) {
+            // Using .size() allows the code to respond to the fact that the ArrayList
+            // is not fixed. The code will stay flexible and prevent bugs.
+            sumOfAverageDailyTemps = sumOfAverageDailyTemps + avgDailyTemp.get(i);
+        }
+
+        // Calculating this outside of the loop is necessary to keep the formula accurate.
+        averageDailyTempForWeek = sumOfAverageDailyTemps / avgDailyTemp.size();
+
+        // Data Retrieval Phase
+        System.out.print("Type a day to view its average temperature, 'week' for the weekly summary, or 'done' to exit: ");
+        userInput = scanner.nextLine();
+        userInput = userInput.toLowerCase();
+
+        while (!userInput.equals("done")) {
+
+            if (userInput.equals("week")) {
+                for (int i = 0; i < daysOfWeek.size(); i++) {
+                    System.out.println(daysOfWeek.get(i) + ": " + avgDailyTemp.get(i));
+                }
+                System.out.println("Weekly Average: " + averageDailyTempForWeek);
+            }
+            else if (daysOfWeek.contains(userInput)) {
+                retrievalIndex = daysOfWeek.indexOf(userInput);
+                System.out.println(daysOfWeek.get(retrievalIndex) + ": " + avgDailyTemp.get(retrievalIndex));
+            }
+            else {
+                System.out.println("Value not recognized. Double check spelling or type 'done' to exit.");
+            }
+
+            // Re-prompt for userInput to prevent infinite loop from occurring.
+            System.out.print("Enter a day, 'week', or 'done': ");
+            userInput = scanner.nextLine();
+            userInput = userInput.toLowerCase();
+        }
 
     }
 }
